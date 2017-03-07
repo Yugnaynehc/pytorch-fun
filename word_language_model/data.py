d3e26bc84a9c0879f6230d2b30f273f0d0759784
Yugnaynehc <cyyyyc123@hotmail.com>
2017-03-07 20:34:02 +0800

    add some test data

---
+++ word_language_model/data.py
@@ -0,0 +1,50 @@
+import os
+import torch
+
+
+class Dictionary(object):
+
+    def __init__(self):
+        self.word2idx = {'<EOS>': 0}
+        self.idx2word = ['<EOS']
+        self.n_words = 1
+
+    def add_word(self, w):
+        if w not in self.word2idx:
+            self.word2idx[w] = self.n_words
+            self.idx2word.append(w)
+            self.n_words += 1
+        return self.word2idx[w]
+
+    def __len__(self):
+        return self.n_words
+
+
+class Corpus(object):
+
+    def __init__(self, path):
+        self.dictionary = Dictionary()
+        self.train = self.tokenize(os.path.join(path, 'train.txt'))
+        self.valid = self.tokenize(os.path.join(path, 'valid.txt'))
+        self.test = self.tokenize(os.path.join(path, 'test.txt'))
+
+    def tokenize(self, path):
+        assert os.path.exists(path)
+
+        n_tokens = 0
+        with open(path, 'r') as f:
+            for line in f:
+                words = line.split() + ['<EOS>']
+                n_tokens += len(words)
+                for w in words:
+                    self.dictionary.add_word(w)
+
+        ids = torch.LongTensor(n_tokens)
+        with open(path, 'r') as f:
+            token = 0
+            for line in f:
+                words = line.split() + ['<EOS>']
+                for w in words:
+                    ids[token] = self.dictionary.word2idx[w]
+                    token += 1
+        return ids
--- word_language_model/model.py
+++ word_language_model/model.py
@@ -2,11 +2,12 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
+
 class RNNModel(nn.Module):
 
     def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers):
         super(RNNModel, self).__init__()
-        self.encoder = nn.Embedding(ntoker, ninp)
+        self.encoder = nn.Embedding(ntoken, ninp)
         self.rnn = getattr(nn, rnn_type)(ninp, nhid, nlayers, bias=False)
         self.decoder = nn.Linear(nhid, ntoken)
 
@@ -16,4 +17,6 @@ class RNNModel(nn.Module):
         self.nhid = nhid
         self.nlayers = nlayers
 
-    def 
+    def forward(self, x, hidden):
+        embedded = self.encoder(x)
+        
